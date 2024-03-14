c46b5751a4f82152d0ab64689aada39596b55580
roberto <roberto.song940726@gmail.com>
2024-03-14 20:33:18 +0900

    memo and usememo demo

---
+++ src/components/memo_demo/Index.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import Todos from "./Todos";
+
+const MemoDemo = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState(["todo 1", "todo 2"]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+
+  return (
+    <>
+      <Todos todos={todos} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
+
+export default MemoDemo;
